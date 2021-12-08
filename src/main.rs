4685b6f88b9a06eb809afcd15ba72b2addd270f1
Marcelo Nicolas Gomez Rivera <mng@Marcelos-MacBook-Pro.local>
2021-12-08 10:46:01 +0000

    Copy diagram

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 // Advent of Code 2021, day 8
 // https://adventofcode.com/2021/day/8
 
+//  0:      1:      2:      3:      4:
+//  aaaa    ....    aaaa    aaaa    ....
+// b    c  .    c  .    c  .    c  b    c
+// b    c  .    c  .    c  .    c  b    c
+//  ....    ....    dddd    dddd    dddd
+// e    f  .    f  e    .  .    f  .    f
+// e    f  .    f  e    .  .    f  .    f
+//  gggg    ....    gggg    gggg    ....
+// 
+//   5:      6:      7:      8:      9:
+//  aaaa    aaaa    aaaa    aaaa    aaaa
+// b    .  b    .  .    c  b    c  b    c
+// b    .  b    .  .    c  b    c  b    c
+//  dddd    dddd    ....    dddd    dddd
+// .    f  e    f  .    f  e    f  .    f
+// .    f  e    f  .    f  e    f  .    f
+//  gggg    gggg    ....    gggg    gggg
+
 fn main() {
     println!("Hello, world!");
 }
